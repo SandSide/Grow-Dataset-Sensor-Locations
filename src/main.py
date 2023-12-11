29fe52a9eef9e067803137f675ea44230f8117ed
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-11 07:12:52 +0000

    refactor: put logic behind functions

--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as image
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
 
-df = pd.read_csv('GrowLocations.csv')
-
 # Ranges
 minLong= -10.592
 maxLong = 1.6848
@@ -13,32 +11,42 @@ maxLong = 1.6848
 minLat = 50.681
 maxLat = 57.985
 
-relevant_data = []
+def cleanData(data):
 
-# Story only relevant data
-df = df[['Latitude', 'Longitude']]
+    # Story only relevant data
+    df = data[['Latitude', 'Longitude']]
 
-# Remove duplicates
-df = df.drop_duplicates()
+    # Remove duplicates
+    df = df.drop_duplicates()
 
-# Switch columns to correct place
-df = df.rename(columns={'Latitude': 'temp', 'Longitude': 'Latitude'})
-df = df.rename(columns={'temp': 'Longitude'})
+    # Switch columns names to correct place
+    df = df.rename(columns={'Latitude': 'temp', 'Longitude': 'Latitude'})
+    df = df.rename(columns={'temp': 'Longitude'})
 
-# Filter lat and lang based on min and max values
-df = df[(df['Latitude'] >= minLat) & (df['Latitude'] <= maxLat) & (df['Longitude'] >= minLong) & (df['Longitude'] <= maxLong)]
+    # Filter lat and lang based on min and max values
+    df = df[(df['Latitude'] >= minLat) & (df['Latitude'] <= maxLat) & (df['Longitude'] >= minLong) & (df['Longitude'] <= maxLong)]
+    
+    return df
 
-# Plot graph
-x = np.array(df['Longitude'])
-y = np.array(df['Latitude'])
+def plotGraph(data):
+    
+    # Get points
+    x = np.array(df['Longitude'])
+    y = np.array(df['Latitude'])
 
-img = image.imread('map7.png')
+    # Manipulate img
+    img = image.imread('map7.png')
+    fig, ax = plt.subplots()
+    ax.imshow(img, extent=[minLong, maxLong, minLat, maxLat])
 
-fig, ax = plt.subplots()
-ax.imshow(img, extent=[minLong, maxLong, minLat, maxLat])
+    # Plot Graph
+    plt.plot(x, y,  'o')
+    plt.xlim(minLong, maxLong)
+    plt.ylim(minLat, maxLat)
 
-plt.plot(x, y,  'o')
-plt.xlim(minLong, maxLong)
-plt.ylim(minLat, maxLat)
+    plt.show()
 
-plt.show()
+df = pd.read_csv('GrowLocations.csv')
+df = cleanData(df)
+plotGraph(df)
+
